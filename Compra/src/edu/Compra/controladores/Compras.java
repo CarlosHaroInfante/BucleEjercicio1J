5eae3233a3b826b6dd4d8da1c16ae0ad7a8f1424
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2023-11-17 09:19:49 +0100

    Add files via upload

---
+++ Compra/src/edu/Compra/controladores/Compras.java
@@ -0,0 +1,25 @@
+package edu.Compra.controladores;
+
+public class Compras {
+
+	public static void main(String[] args) {
+		
+		
+		int meses = 1;
+		
+		
+		for(long pago = 10; meses <= 20 ; ++meses ) 
+		{
+			System.out.println(pago);
+			pago = (pago * 2);
+			
+			
+		}
+		
+			
+		
+
+	}
+
+}
+
